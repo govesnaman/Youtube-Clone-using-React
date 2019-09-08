c2f6520c65f1eb9ee0b8a4811a78290f76365713
Naman Govila <namangovila@gmail.com>
2019-09-08 18:47:31 +0530

    Added like functionality and form for comments

--- src/Components/PlayerDetails.js
+++ src/Components/PlayerDetails.js
@@ -1,6 +1,7 @@
 import React  from "react";
 import "../Styles/PlayerDetails.css";
 import Heart from "./Heart";
+import CommentBox from "./CommentBox";
 
 // import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
@@ -12,21 +13,34 @@ const PlayerDetails = ({ selectedVideo }) => {
   const videoSrc = `https://www.youtube.com/embed/${selectedVideo.id.videoId}`;
   
   const { description, title, channelTitle } = selectedVideo.snippet;
-     
   
+
+
+
   return (
     <div>
       <div className="player">
         <iframe id = "main" title={description} src={videoSrc} frameBorder="0" height="450px" width= "100%"/>
       </div>
       <div className="text">
+      <Heart />
         <h4>{title}</h4>
-        <Heart/>
         
-
-        <p>{channelTitle}</p>
-        <p> Comments </p>
+        <p className = "grey">{channelTitle}</p>
       </div>
+      
+     <p className="comments"> Comments </p>
+     <div className="textboxes">
+    <form>
+     <input type="text" className="writer" placeholder="Author"/>
+     <input type="text" className="writer" placeholder="Comment"/>
+     <div className="buttons">
+     <input className="btnss" type="Submit" value="Comment"/>
+     <input className="btnss" type="button" value="Cancel"/>
+     </div>
+     </form>
+     <CommentBox/>
+     </div>
     </div>
   );
 };
